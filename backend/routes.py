b11362be86c4d744e75ca6ffc4985f2ca1b6edff
mahlahj <37989755+mahlahj@users.noreply.github.com>
2023-12-31 20:00:56 -0600

    initial commit

---
+++ backend/routes.py
@@ -0,0 +1,105 @@
+from . import app
+import os
+import json
+from flask import jsonify, request, make_response, abort, url_for  # noqa; F401
+
+SITE_ROOT = os.path.realpath(os.path.dirname(__file__))
+json_url = os.path.join(SITE_ROOT, "data", "pictures.json")
+data: list = json.load(open(json_url))
+
+
+######################################################################
+# RETURN HEALTH OF THE APP
+######################################################################
+@app.route("/health")
+def health():
+    return jsonify(dict(status="OK")), 200
+
+
+######################################################################
+# COUNT THE NUMBER OF PICTURES
+######################################################################
+@app.route("/count")
+def count():
+    """return length of data"""
+    if data:
+        return jsonify(length=len(data)), 200
+
+    return {"message": "Internal server error"}, 500
+
+
+######################################################################
+# GET ALL PICTURES
+######################################################################
+@app.route("/picture", methods=["GET"])
+def get_pictures():
+    return data, 200
+
+
+######################################################################
+# GET A PICTURE
+######################################################################
+@app.route("/picture/<int:id>", methods=["GET"])
+def get_picture_by_id(id):
+
+    for pict in data:
+        if pict["id"] == id:
+            return pict, 200
+
+    return {"message": f"Picture with id {id} not found"}, 404
+
+
+######################################################################
+# CREATE A PICTURE
+######################################################################
+@app.route("/picture", methods=["POST"])
+def create_picture():
+
+    new_picture = request.json
+    if not new_picture:
+        return {"message": "No data supplied"}, 422
+
+    if new_picture.get("id", None) is None:
+        return {"message": "Supplied data is missing picture id"}, 422
+
+    for pict in data:
+        if pict["id"] == new_picture["id"]:
+            return {"message": f"picture with id {new_picture['id']} already present"}, 302
+
+    data.append(new_picture)
+    return new_picture, 201
+
+
+######################################################################
+# UPDATE A PICTURE
+######################################################################
+@app.route("/picture/<int:id>", methods=["PUT"])
+def update_picture(id):
+
+    new_picture = request.json
+    if not new_picture:
+        return {"message": "No data supplied"}, 422
+
+    if new_picture.get("id", None) is None:
+        return {"message": "Supplied data is missing picture id"}, 422
+
+    for idx, pict in enumerate(data):
+        if pict["id"] == id:
+            data[idx] = new_picture
+            return new_picture, 200
+
+    return {"message": "Picture with id {id} not found"}, 404
+
+
+######################################################################
+# DELETE A PICTURE
+######################################################################
+@app.route("/picture/<int:id>", methods=["DELETE"])
+def delete_picture(id):
+
+    for idx, pict in enumerate(data):
+        if pict["id"] == id:
+            del data[idx]
+            return "", 204
+
+    return {"message": "Picture with id {id} not found"}, 404
